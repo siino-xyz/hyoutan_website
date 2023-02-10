3746e86136b71416f31e8364971d314dfc69892d
siino-xyz <b114807h@gmail.com>
2023-02-10 19:22:21 +0900

    prettier インストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
+            Find あいうえお information about Next.js features and API.
           </p>
         </a>
 
